ac68161140dae9276916572e0e6e0919c8ec4f3c
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-02-22 18:49:22 +0100

    Automatic installation of PIL dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 readme = open('README.rst').read()
 
 setup(name='googlegantt',
-        version='0.6',
+        version='0.6.1',
         author='Adam Bard',
         author_email='adam@adambard.com',
         license='MIT',
         description='Produce Gantt charts using the Google Charts API',
         long_description=readme,
-        py_modules=['googlegantt'])
+        py_modules=['googlegantt'],
+        install_requires=[
+            "PIL",
+        ],
+)
 
